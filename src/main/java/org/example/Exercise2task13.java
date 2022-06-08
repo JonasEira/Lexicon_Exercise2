b19e0e754e05ec34cd30368b24386f9c94c0d4c5
Jonas Eira <jonas.eira@gmail.com>
2022-06-08 20:11:49 +0200

    attempt starting at 13th task in the 2nd exercise

---
+++ src/main/java/org/example/Exercise2task13.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Exercise2task13 {
+    private int[] arr;
+    private final int length = 342;
+    private int[] emptyArr;
+
+    public Exercise2task13() {
+        this.arr = new int[this.length];
+        for(int n = 0; n < arr.length; n++){
+            arr[n] = (int)(Math.random()*1000.0d)
+        }
+        this.emptyArr = new int[this.length]
+    }
+
+    public int[] sortArray(){
+
+    }
+}
