f301d2254de50bb461254cdc85e29e6e0543077f
Jonas Eira <jonas.eira@gmail.com>
2022-06-08 20:12:31 +0200

    attempt starting at 13th task in the 2nd exercise

--- src/main/java/org/example/Exercise2task13.java
+++ src/main/java/org/example/Exercise2task13.java
@@ -5,6 +5,7 @@ public class Exercise2task13 {
     private final int length = 342;
     private int[] emptyArr;
 
+    // Adding random numbers of 0 - 1000 to a 342 length int vector
     public Exercise2task13() {
         this.arr = new int[this.length];
         for(int n = 0; n < arr.length; n++){
